6ebb0c3dc8951e8af1aa53655f39d6c33336846d
Solswap Admin <admin@solswap.cloud>
2026-04-09 14:33:47 +0000

    feat: add FAQ, Donation modal (with XMR), and Matrix badge

--- changenow-proxy.js
+++ changenow-proxy.js
@@ -36,7 +36,8 @@ http.createServer((req, res) => {
     method: req.method,
     headers: { 
       'Content-Type': 'application/json',
-      'x-changenow-api-key': '353e12df6ccc210ab17c8cc917aad2aa47b84cd76e764c8dd27944dfb150f60d'
+      'x-changenow-api-key': '353e12df6ccc210ab17c8cc917aad2aa47b84cd76e764c8dd27944dfb150f60d',
+      'x-changenow-apikey': '353e12df6ccc210ab17c8cc917aad2aa47b84cd76e764c8dd27944dfb150f60d'
     }
   };
 
