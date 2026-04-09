6f0eecae763ddb5622c909fca3cec983e354e872
Solswap Admin <admin@solswap.cloud>
2026-04-09 16:21:43 +0000

    feat: Migrate MoneroSwap to Astro and configure CI/CD

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://moneroswap.cloud',
+  outDir: './dist',
+  publicDir: './public',
+});
